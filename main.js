4acca90a3c7a56dee7c57a14a23042ce1f12c2f2
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2019-01-22 19:41:43 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function change(x,y,char) {
 	let game = document.getElementById("game").innerHTML;
-	let pos = y*10+x+1;
+	let pos = y*10+x-12;
 	game = game.slice(0,pos) + char + game.slice(pos+1);
 	document.getElementById("game").innerHTML = game;
 	
@@ -24,16 +24,16 @@ let player = {
 			this.yPos += 1;
 		}
 		if(this.x < 0) {
-			this.x = 0;
+			this.xPos = 0;
 		}
 		if(this.y < 0) {
-			this.y = 0;
+			this.yPos = 0;
 		}
 		if(this.x > 9) {
-			this.x = 9;
+			this.xPos = 9;
 		}
 		if(this.y > 9) {
-			this.y = 9;
+			this.yPos = 9;
 		}
 		change(this.xPos,this.yPos,"@");
 	}
