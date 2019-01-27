f887cd8ecdb482a0e85398e6c7cb451a4a35f6a6
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2019-01-27 15:35:24 -0600

    Update monsters.js

--- monsters.js
+++ monsters.js
@@ -42,7 +42,7 @@ function pathfind(object,target) {
 				}
 			}
 		}
-		object.move(
+		object.move(path2[0],path2[1]);
 	}
 }
 function pathfindHelper(array,x,y) {
@@ -107,11 +107,92 @@ function pathfindHelper2(array,x,y) {
 	return coords[Math.floor(Math.random()*coords.length)];
 }
 class Monster extends Thing {
-	constructor(char,color,map,x,y,ai,thePlayer) {
+	constructor(char,color,map,x,y,ai,thePlayer,fighter,name) {
 		super(char,color,map,x,y);
 		this.ai = function() {
 			ai(this,thePlayer);
 		}
+		this.fighter = fighter;
+		this.name = name;
+		this.move = function(dx,dy) {
+			if(this.map.world) {
+				let x = this.chunkPosX;
+				let y = this.chunkPosY;
+				if(this.chunkPosX+dx > chunkSize) {
+					new Chunk(this.map.posX+1,this.map.posY,this.map.world);
+					let thing = this.map.world.map[this.map.posX+1][this.map.posY].map[x+dx-chunkSize][y+dy];
+					if(thing.fighter) {
+						this.fighter.attack(thing.fighter);
+					} else if(!(thing === "wall")) {
+						this.map.map[x][y] = "empty";
+						this.map = this.map.world.map[this.map.posX+1][this.map.posY];
+						this.map.map[x+dx-chunkSize][y+dy] = this;
+					}
+				} else if(this.chunkPosY+dy > chunkSize) {
+					new Chunk(this.map.posX,this.map.posY+1,this.map.world);
+					let thing = this.map.world.map[this.map.posX][this.map.posY+1].map[x+dx][y+dy+chunkSize];
+					if(thing.fighter) {
+						this.fighter.attack(thing.fighter);
+					} else if(!(thing === "wall")) {
+						this.map.map[x][y] = "empty";
+						this.map = this.map.world.map[this.map.posX][this.map.posY+1];
+						this.map.map[x+dx][y+dy-chunkSize] = this;
+					}
+				} else if(this.chunkPosX+dx < 1) {
+					new Chunk(this.map.posX-1,this.map.posY,this.map.world);
+					let thing = this.map.world.map[this.map.posX-1][this.map.posY].map[x+dx+chunkSize][y+dy];
+					if(thing.fighter) {
+						this.fighter.attack(thing.fighter);
+					} else if(!(thing === "wall")) {
+						this.map.map[x][y] = "empty";
+						this.map = this.map.world.map[this.map.posX-1][this.map.posY];
+						this.map.map[x+dx+chunkSize][y+dy] = this;
+					}
+				} else if(this.chunkPosY+dy < 1) {
+					new Chunk(this.map.posX,this.map.posY-1,this.map.world);
+					let thing = this.map.world.map[this.map.posX][this.map.posY-1].map[x+dx][y+dy+chunkSize];
+					if(thing.fighter) {
+						this.fighter.attack(thing.fighter);
+					} else if(!(thing === "wall")) {
+						this.map.map[x][y] = "empty";
+						this.map = this.map.world.map[this.map.posX][this.map.posY-1];
+						this.map.map[x+dx][y+dy+chunkSize] = this;
+					}
+				} else {
+					let thing = this.map.map[x+dx][y+dy];
+					if(thing.fighter) {
+						this.fighter.attack(thing.fighter);
+					} else if(!(thing === "wall")) {
+						this.map.map[x][y] = "empty";
+						this.map.map[x+dx][y+dy] = this;
+					}
+				}
+			} else if(!(this.posX+dx < 1 || this.posY+dy < 1 || this.posX+dx > this.map.sizeX || this.posY+dy > this.map.sizeY)) { 
+				let thing = this.map.map[this.posX+dx][this.posY+dy];
+				if(thing.fighter) {
+					this.fighter.attack(thing.fighter);
+				} else if(!(thing === "wall")) {
+					let x = this.posX;
+					let y = this.posY;
+					this.map.map[x][y] = "empty";
+					this.map.map[x+dx][y+dy] = this;
+				}
+			}
+		}
 	}
 }
-new Monster("%","#000000",map,18,18,pathfind,player);
+class Fighter extends Thing {
+	constructor(hp,power) {
+		this.hp = hp;
+		this.power = power;
+		this.attack = function(enemy) {
+			enemy.hp -= this.power;
+		}
+	}
+}
+player.fighter = new Fighter(30,3);
+class LargeRat extends Monster{
+	constructor(x,y) {
+		super("%","#000000",map,x,y,pathfind,player,new Fighter(10,1),"Large Rat");
+	}
+}
