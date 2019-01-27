cacfc3abc5f96de9b2b499900293276c1dd04554
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2019-01-27 12:42:10 -0600

    Update monsters.js

--- monsters.js
+++ monsters.js
@@ -1,3 +1,98 @@
-function pathfind() {
-	
+let marker = new Tile("#","0000ff",null,"debug");
+function pathfind(object,target) {
+	if(object.inCamera) {
+		let array = [];
+		for(let i = 1; i <= camera.size; i++) {
+			array.push([]);
+			for(let j = 1; j<= camera.size; j++) {
+				array[i].push(camera[i][j]);
+			}
+		}
+		let x = object.relPosX;
+		let y = object.relPosY;
+
+		for(let i in array) {
+			for(let j in array[i]) {
+				if(array[i][j].type = "wall" || array[i][j].ai) {
+					array[i][j] = 9**9;
+				}
+			}
+		}
+		array[x][y] = 0;
+		let arrayCoords = [[x,y]];
+		for(let i = 0; i < arrayCoords.length; i++) {
+			arrayCoords.concat(pathfindHelper(array,arrayCoords[i][0],arrayCoords[i][1]));
+		}
+		let path = [target.relPosX,target.relPosY];
+		if(typeof arrayCoords[path[0]][path[1]] === "number") {
+			while(arrayCoords[path[0]][path[1]] > 0) {
+				path = pathfindHelper2(array,path[0],path[1]);
+				if(arrayCoords[path[0]][path[1]] > 0) {
+					camera[path[0]][path[1]] = marker;
+				}
+			}
+		}
+	}
 }
+function pathfindHelper(array,x,y) {
+	let coords = [];
+	if(array[x-1][y]) {
+		if(typof array[x-1][y] != "number") {
+			array[x-1][y] = array[x][y]+1;
+			coords.push([x-1,y]);
+		}
+	}
+	if(array[x+1][y]) {
+		if(typof array[x+1][y] != "number") {
+			array[x+1][y] = array[x][y]+1;
+			coords.push([x+1,y]);
+		}
+	}
+	if(array[x][y-1]) {
+		if(typof array[x][y-1] != "number") {
+			array[x][y-1] = array[x][y]+1;
+			coords.push([x,y-1]);
+		}
+	}
+	if(array[x][y+1]) {
+		if(typof array[x][y+1] != "number") {
+			array[x][y+1] = array[x][y]+1;
+			coords.push([x,y+1]);
+		}
+	}
+	return coords;
+}
+
+function pathfindHelper2(array,x,y) {
+	let coords = [];
+	if(array[x-1][y]) {
+		if(array[x-1][y] === array[x][y]-1) {
+			coords.push([x-1,y]);
+		}
+	}
+	if(array[x+1][y]) {
+		if(array[x+1][y] === array[x][y]-1) {
+			coords.push([x+1,y]);
+		}
+	}
+	if(array[x][y-1]) {
+		if(array[x][y-1] === array[x][y]-1) {
+			coords.push([x,y-1]);
+		}
+	}
+	if(array[x][y+1]) {
+		if(array[x][y+1] === array[x][y]-1) {
+			coords.push([x,y+1]);
+		}
+	}
+	return coords[Math.floor(Math.random()*coords.length)];
+}
+class Monster extends Thing {
+	constructor(char,color,map,x,y,ai,thePlayer) {
+		super(char,color,map,x,y);
+		this.ai = function() {
+			ai(this,thePlayer);
+		}
+	}
+}
+new Monster("%","#000000",map,18,18,pathfind,player);
