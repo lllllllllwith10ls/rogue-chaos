19dd70767cd908b7168470963bc249de4c54221d
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2019-01-27 20:36:41 -0600

    Update monsters.js

--- monsters.js
+++ monsters.js
@@ -211,9 +211,9 @@ class RatAi {
 	move() {
 		this.observe();
 		if(this.notables[0]) {
-			let array = dijkstra(this.parent,this.notables[0].thing,this.notables[0].desire);
+			let array = dijkstra(this.notables[0].thing,this.notables[0].desire);
 			for(let i = 1; i < this.notables.length; i++) {
-				let array2 = dijkstra(this.parent,this.notables[i].thing,this.notables[i].desire)
+				let array2 = dijkstra(this.notables[i].thing,this.notables[i].desire)
 				for(let j = 1; j < array.length; j++) {
 					for(let k = 1; k < array.length; k++) {
 						array[j][k] += array2[j][k];
