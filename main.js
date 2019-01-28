f4f3c670014629b2cdbecd9b14810a0838a7169b
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2019-01-27 22:20:04 -0600

    Update main.js

--- main.js
+++ main.js
@@ -275,6 +275,7 @@ camera.draw = function() {
 				}
 			}
 		}
+		console.log(camera);
 		for(let i = 1; i <= camera.size; i++) {
 			for(let j = 1; j <= camera.size; j++) {
 				if(this[i][j].ai) {
