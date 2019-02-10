18ab965eb8f3f91c131da8aeb2ecb4f7c2a992df
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2019-02-09 21:43:42 -0600

    Update main.js

--- main.js
+++ main.js
@@ -205,22 +205,22 @@ function move(dir) {
 	
 	if(player.relPosX < camera.size/2+.5-3) {
 		if(!(camera.x + player.relPosX-camera.size/2+.5-3 < 1)) {
-			camera.x+=player.relPosX-camera.size/2+.5-3;
+			camera.x+=player.relPosX-(camera.size/2+.5-3);
 		}
 	}
 	if(player.relPosY < camera.size/2+.5-3) {
 		if(!(camera.y + player.relPosY-camera.size/2+.5-3 < 1)) {
-			camera.y+=player.relPosY-camera.size/2+.5-3;
+			camera.y+=player.relPosY-(camera.size/2+.5-3);
 		}
 	}
 	if(player.relPosX > camera.size/2+.5+3) {
 		if(!(camera.x + player.relPosX+1 > player.map.sizeX)) {
-			camera.x+=player.relPosX-camera.size/2+.5+3;
+			camera.x+=player.relPosX-(camera.size/2+.5+3);
 		}
 	}
 	if(player.relPosY > camera.size/2+.5+3) {
 		if(!(camera.y + player.relPosY+1 > player.map.sizeX)) {
-			camera.y+=player.relPosY-camera.size/2+.5+3;
+			camera.y+=player.relPosY-(camera.size/2+.5+3);
 		}
 	}
 	let log = document.getElementById("log");
